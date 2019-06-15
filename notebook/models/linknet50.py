7f8ca237aadcc01a03975e9b8bc211af11166a9f
thibaultroyet <thibaultroyet@github.com>
2019-06-15 18:59:34 +0000

    changing name

--- notebook/models/linknet50.py
+++ notebook/models/linknet50.py
@@ -8,7 +8,7 @@ from keras.layers import BatchNormalization, UpSampling2D, Dropout, AvgPool2D, S
 def decoder_block(prevlayer, filters, prefix, strides=(1, 1)):
     conv = Conv2D(filters, (1, 1), padding="same", strides=strides, name=prefix + "_conv1")(prevlayer)
     conv = BatchNormalization(name=prefix + "_bn1")(conv)
-    conv = Conv2DTranspose(filters, (4, 4), strides=(2, 2), padding='same', name=prefix + "_Transp_conv")
+    conv = Conv2DTranspose(filters, (4, 4), strides=(2, 2), padding='same', name=prefix + "_Transp_conv")(conv)
     conv = BatchNormalization(name=prefix + "_bn2")(conv)
     conv = Conv2D(filters, (1, 1), padding="same", strides=strides, name=prefix + "_conv2")(conv)
     conv = BatchNormalization(name=prefix + "_bn3")(conv)
@@ -16,7 +16,7 @@ def decoder_block(prevlayer, filters, prefix, strides=(1, 1)):
     return conv
 
 
-def get_unet_resnet50(input_shape):
+def get_linknet50(input_shape):
     resnet_base = ResNet50(input_shape=input_shape, include_top=False, weights='imagenet')
 
     for l in resnet_base.layers:
@@ -27,22 +27,22 @@ def get_unet_resnet50(input_shape):
     conv4 = resnet_base.get_layer("activation_40").output
     conv5 = resnet_base.get_layer("activation_49").output
 
-    up6 = concatenate([(conv5), conv4], axis=-1)
-    conv6 = decoder_block(up6, 1024, "conv6_1")
+    
+    deconv1 = decoder_block(conv5, 1024, "deconv1")
+    concat1 = concatenate([deconv1, conv4], axis=-1)
+    
+    deconv2 = decoder_block(concat1, 512, "deconv2")
+    concat2 = concatenate([deconv2, conv3], axis=-1)
+    
+    deconv3 = decoder_block(concat2, 256, "deconv3")
+    concat3 = concatenate([deconv3, conv2], axis=-1)
 
-    up7 = concatenate([(conv6), conv3], axis=-1)
-    conv7 = decoder_block(up7, 512, "conv7_1")
+    deconv4 = decoder_block(deconv3, 128, "deconv4")
 
-    up8 = concatenate([(conv7), conv2], axis=-1)
-    conv8 = decoder_block(up8, 256, "conv8_1")
-
-    up9 = concatenate([(conv8), conv1], axis=-1)
-    conv9 = decoder_block(up9, 128, "conv9_1")
-
-    up10 = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same', name=prefix + "_Transp_conv")(conv9)
-    conv10 = Conv2D(64, (3, 3), padding="same", strides=(1,1), name="conv11")(up10)
-    conv11 = Conv2D(64, (2, 2), padding="same", strides=(1,1), name="conv11")(up10)
+    deconv5 = Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same', name="deconv5")(deconv4)
+    conv10 = Conv2D(64, (3, 3), padding="same", strides=(1,1), name="conv10")(deconv5)
+    conv11 = Conv2D(64, (2, 2), padding="same", strides=(1,1), name="conv11")(conv10)
     # conv10 = SpatialDropout2D(0.2)(conv10)
-    x = Conv2D(1, (1, 1), activation="sigmoid", name="prediction")(conv10)
+    x = Conv2D(1, (1, 1), activation="sigmoid", name="prediction")(conv11)
     model = models.Model(resnet_base.input, x)
     return model
